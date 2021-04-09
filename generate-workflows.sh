abae24bedf54d2eb65c61c16a457c606305e7308
Francesco Ciocchetti <primeroznl@gmail.com>
2021-04-09 14:58:32 +0200

    Test building for both dockerhub and quay

---
+++ generate-workflows.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+generate_workflow() {
+  DOCKERFILE_NAME="$1"
+  TYPE="$2"
+ 
+  CONTEXT_PATH=$(dirname "$DOCKERFILE_NAME")
+  IMAGE_NAME="$TYPE / $(echo "$CONTEXT_PATH" | tr '/' ' ' | awk '{print $2" "$3}')"
+  WORKFLOW_FILE_NAME="on-push-main-$(echo "$CONTEXT_PATH" | tr '/' '-').yml"
+
+  TEMPLATES=()
+  TEMPLATES+=(".github/templates/on-push-main-PRE.yml")
+
+  if [[ "x$TYPE" == "xdocker" ]]; then
+    TEMPLATES+=(".github/templates/dockerhub-registry.yml")
+  elif [[ "x$TYPE" == "xquay" ]]; then
+    TEMPLATES+=(".github/templates/quay-registry.yml")
+  fi
+
+  TEMPLATES+=(".github/templates/build-publish.yml")
+
+  echo "Generating $WORKFLOW_FILE_NAME"
+
+	cat $(echo "${TEMPLATES[*]}") | CONTEXT_PATH=${CONTEXT_PATH} IMAGE_NAME=${IMAGE_NAME} envsubst > ".github/workflows/$WORKFLOW_FILE_NAME"
+}
+
+docker_workflows() {
+	find docker/ -type f -name Dockerfile | while read -r f; do
+    generate_workflow "$f" "docker"
+	done
+}
+
+quay_workflows() {
+	find quay/ -type f -name Dockerfile | while read -r f; do
+    generate_workflow "$f" "quay"
+	done
+}
+
+
+docker_workflows
+quay_workflows
---
+++ quay/nmaupu/vault-secret/noop.md
@@ -0,0 +1,6 @@
+1. a noop file to trigger builds
+2. a noop file to trigger builds
+3. a noop file to trigger builds
+4. a noop file to trigger builds
+5. a noop file to trigger builds
+6. a noop file to trigger builds
